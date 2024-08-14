d5e2053baf613a9461cac3c2dbc72b212094aa31
Artur <71281020+Artur0412-KU@users.noreply.github.com>
2024-08-14 21:54:54 +0300

    modified:   src/components/Header/Search/Search.tsx

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './search.module.scss';
 import { Input } from 'antd';
 import SearchIcon from './SearchIcon';
