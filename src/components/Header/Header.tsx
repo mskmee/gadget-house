ede05a5e09afa842c1ec2f01535ff81e246df4ab
Sosnovskyi <roman97039703@gmail.com>
2024-08-24 18:09:36 +0200

    MV

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './header.module.scss';
 import { CatalogIcon } from '@/assets/constants';
 import Search from './Search/Search';
@@ -5,54 +6,50 @@ import ButtonNav from '../Button/Button';
 import buttonData from '@/constants/ButtonConstants';
 import { useMediaQuery } from 'react-responsive';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
-import { Link } from 'react-router-dom';
 import { AppRoute } from '@/enums/Route';
 
 export default function Header() {
-  const isMaxWidth1070 = useMediaQuery({
-    query: '(max-width: 1070px)',
-  });
+  const isMaxWidth1070 = useMediaQuery({ query: '(max-width: 1070px)' });
+
   return (
-    <>
+    <header>
       <div className={styles.headerTop}>
-        <Link to={AppRoute?.ROOT} className={styles.headerTopLogo}>
+        <a href={AppRoute?.ROOT} className={styles.headerTopLogo}>
           GadgetHouse
-        </Link>
+        </a>
       </div>
       <div className={styles.headerBottom}>
-        {!isMaxWidth1070 && (
+        {!isMaxWidth1070 ? (
           <div className={styles.headerBottomCatalog}>
-            <img src={CatalogIcon} />
+            <img src={CatalogIcon} alt="Catalog" />
             <h1>Catalog</h1>
           </div>
+        ) : (
+          <BurgerMenu />
         )}
-        {isMaxWidth1070 && <BurgerMenu />}
         <Search />
-
         <div className={styles.headerBottomButtons}>
           {isMaxWidth1070
-            ? buttonData.slice(3, 4).map((item) => {
-                return (
+            ? buttonData
+                .slice(3, 4)
+                .map((item) => (
                   <ButtonNav
                     key={item.id}
                     icon={item.img}
                     hoverImg={item.hoverImg}
                     clickImg={item.clickImg}
                   />
-                );
-              })
-            : buttonData.map((item) => {
-                return (
-                  <ButtonNav
-                    key={item.id}
-                    icon={item.img}
-                    hoverImg={item.hoverImg}
-                    clickImg={item.clickImg}
-                  />
-                );
-              })}
+                ))
+            : buttonData.map((item) => (
+                <ButtonNav
+                  key={item.id}
+                  icon={item.img}
+                  hoverImg={item.hoverImg}
+                  clickImg={item.clickImg}
+                />
+              ))}
         </div>
       </div>
-    </>
+    </header>
   );
 }
