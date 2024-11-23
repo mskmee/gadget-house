993e85aa12f978d007800bd0f2ab22d310122554
Maksim Maksimenko <mskmee2@gmail.com>
2024-11-24 00:05:55 +0200

    fix: react router warnings

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { routes } from './routes';
 import './styles/globals.scss';
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <RouterProvider router={routes} future={{ v7_startTransition: true }} />
+  );
 }
 
 export default App;
