0d43a6a03db69b0490d2bfe6abf0fce57edcdc92
Maksim Maksimenko <mskmee2@gmail.com>
2024-11-24 10:08:54 +0200

    add: products fetch example

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Benefits from '@/components/benefitsList/benefits';
 import { BrandCard } from '@/components/Card/BrandCard';
 import Carousels from '@/components/Carousel/Carousel';
@@ -10,6 +11,8 @@ import {
   previouslyReviewedData,
 } from '@/components/Card/constants';
 import { MainIntro } from '@/components/MainIntro';
+import { useTypedSelector, useActions } from '@/hooks/hooks';
+import { DataStatus } from '@/enums/enums';
 
 const brandConatinerResponsiveSettings = [
   {
@@ -43,68 +46,88 @@ const brandConatinerResponsiveSettings = [
 ];
 
 export default function Main() {
+  const { getAllProducts } = useActions();
+  const isProductsLoading = useTypedSelector(
+    (state) => state.products.dataStatus === DataStatus.PENDING,
+  );
+
+  useEffect(() => {
+    getAllProducts();
+  }, [getAllProducts]);
+
   return (
     <>
+      <button onClick={getAllProducts}>Press me</button>
       <MainIntro />
-      <SliderNav text="Smartphone" link="/smartphones" />
-      <Carousels
-        classname="mobile-carousel"
-        sliderClassName="mobile-slider"
-        countSlideToShow={5}
-      >
-        {Array.from({ length: 8 }, (_, i) => (
-          <MyCard
-            key={`smartphone-${i}`}
-            product={smartphoneData[i % smartphoneData.length]}
-            classname="smartphone"
-          />
-        ))}
-      </Carousels>
+      {isProductsLoading ? (
+        // it's a place holder. Add loader or skeleton here
+        <div style={{ textAlign: 'center', flex: 1 }}>Loading...</div>
+      ) : (
+        <>
+          <SliderNav text="Smartphone" link="/smartphones" />
+          <Carousels
+            classname="mobile-carousel"
+            sliderClassName="mobile-slider"
+            countSlideToShow={5}
+          >
+            {Array.from({ length: 8 }, (_, i) => (
+              <MyCard
+                key={`smartphone-${i}`}
+                product={smartphoneData[i % smartphoneData.length]}
+                classname="smartphone"
+              />
+            ))}
+          </Carousels>
 
-      <Carousels
-        classname="brands-carousel"
-        sliderClassName="brands-slider"
-        countSlideToShow={5}
-        responsive={brandConatinerResponsiveSettings}
-      >
-        {Array.from({ length: 10 }, (_, i) => (
-          <BrandCard
-            key={`brand-${i}`}
-            product={brandData[i % brandData.length]}
-          />
-        ))}
-      </Carousels>
+          <Carousels
+            classname="brands-carousel"
+            sliderClassName="brands-slider"
+            countSlideToShow={5}
+            responsive={brandConatinerResponsiveSettings}
+          >
+            {Array.from({ length: 10 }, (_, i) => (
+              <BrandCard
+                key={`brand-${i}`}
+                product={brandData[i % brandData.length]}
+              />
+            ))}
+          </Carousels>
 
-      <SliderNav text="Laptop" link="/laptops" />
-      <Carousels
-        classname="laptop-carousel"
-        sliderClassName="laptop-slider"
-        countSlideToShow={5}
-      >
-        {Array.from({ length: 8 }, (_, i) => (
-          <MyCard
-            key={`laptop-${i}`}
-            product={laptopData[i % laptopData.length]}
-            classname="laptop"
-          />
-        ))}
-      </Carousels>
+          <SliderNav text="Laptop" link="/laptops" />
+          <Carousels
+            classname="laptop-carousel"
+            sliderClassName="laptop-slider"
+            countSlideToShow={5}
+          >
+            {Array.from({ length: 8 }, (_, i) => (
+              <MyCard
+                key={`laptop-${i}`}
+                product={laptopData[i % laptopData.length]}
+                classname="laptop"
+              />
+            ))}
+          </Carousels>
 
-      <SliderNav text="Previously reviewed offers" link="/viewed" />
-      <Carousels
-        classname="viewed-carousel"
-        sliderClassName="viewed-slider"
-        countSlideToShow={5}
-      >
-        {Array.from(Array(8), (_, i) => (
-          <MyCard
-            key={`reviewed-${i}`}
-            product={previouslyReviewedData[i % previouslyReviewedData.length]}
-            classname="previously-reviewed"
-            index={i}
-          ></MyCard>
-        ))}
-      </Carousels>
+          <SliderNav text="Previously reviewed offers" link="/viewed" />
+          <Carousels
+            classname="viewed-carousel"
+            sliderClassName="viewed-slider"
+            countSlideToShow={5}
+          >
+            {Array.from(Array(8), (_, i) => (
+              <MyCard
+                key={`reviewed-${i}`}
+                product={
+                  previouslyReviewedData[i % previouslyReviewedData.length]
+                }
+                classname="previously-reviewed"
+                index={i}
+              ></MyCard>
+            ))}
+          </Carousels>
+        </>
+      )}
+
       <Benefits />
     </>
   );
--- src/store/products/actions.ts
+++ src/store/products/actions.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { productsService } from '@/utils/packages/products';
 
+// we can use toastify for better user experience, if design is ready
 const getAllProducts = createAsyncThunk('products/fetch', async () => {
   return await productsService.getAllProducts();
 });
