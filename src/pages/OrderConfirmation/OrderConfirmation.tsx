2abb7d46d2bdebebe625c22d1a22ffd1ac03e5c1
Serhii Svitlychnyi <svetlichniy1905@gmail.com>
2025-01-31 10:34:45 +0200

    created component ProductCardForOrder

--- src/pages/OrderConfirmation/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation/OrderConfirmation.tsx
@@ -6,16 +6,13 @@ import {
   ContactsForm,
   DeliveryForm,
   PaymentForm,
+  ProductCardForOrder,
 } from './libs/components/components';
 import { useOrderConfirmation } from './libs/hooks/hooks';
 import { AppRoute } from '@/enums/Route';
 import { convertPriceToReadable } from '@/utils/helpers/product';
-import { useActions } from '@/hooks/useActions';
 import { useTypedSelector } from '@/hooks/useTypedSelector';
-import { useProductCardHandlers } from '@/hooks/useProductCardHandlers';
 import { SuccessPopUp } from './SuccessPopUp';
-
-import { HeartIcon } from '@/assets/icons/HeartIcon';
 
 import styles from './order-confirmation.module.scss';
 
@@ -40,13 +37,9 @@ const OrderConfirmation: FC = () => {
 
   const navigate = useNavigate();
 
-  const { deleteFromStore, increaseItemQuantity, decreaseItemQuantity } =
-    useActions();
-
   const { products, cardTotalAmount, currency, locale } = useTypedSelector(
     (state) => state.shopping_card,
   );
-  const { isLiked, handleClickLike } = useProductCardHandlers();
 
   useEffect(() => {
     if (products.length === 0) {
@@ -92,128 +85,7 @@ const OrderConfirmation: FC = () => {
             <h3 className={styles.order__itemsTitle}>Your order</h3>
             <ul className={styles.order__itemsList}>
               {products.map((product) => (
-                <li className={styles.order__item} key={product.id}>
-                  <article className={styles.order__itemWrapper}>
-                    <div className={styles.order__itemImage}>
-                      <img
-                        src={product.images[0].link}
-                        alt={product.title}
-                        width={100}
-                        height={100}
-                      />
-                    </div>
-
-                    <div className={styles.order__itemInfo}>
-                      <div className={styles.order__itemHeader}>
-                        <h4 className={styles.order__itemTitle}>
-                          {product.title}
-                        </h4>
-
-                        <button
-                          className={styles.order__itemDelete}
-                          type="button"
-                          onClick={() => deleteFromStore(product.id)}
-                        ></button>
-                      </div>
-
-                      <p className={styles.order__itemCode}>
-                        code: {product.code}
-                      </p>
-
-                      <div className={styles.order__itemFooter}>
-                        <div className={styles.order__itemQuantity}>
-                          <button
-                            onClick={() => decreaseItemQuantity(product.id)}
-                          >
-                            -
-                          </button>
-                          <p>{product.quantity}</p>
-                          <button
-                            onClick={() => increaseItemQuantity(product.id)}
-                          >
-                            +
-                          </button>
-                        </div>
-
-                        <p className={styles.order__itemPrice}>
-                          {convertPriceToReadable(
-                            product.totalPrice,
-                            currency,
-                            locale,
-                          )}
-                        </p>
-                      </div>
-                    </div>
-                  </article>
-
-                  <article className={styles.order__itemWrapper_mobile}>
-                    <div className={styles.order__itemInfo}>
-                      <div className={styles.order__itemImage}>
-                        <img
-                          src={product.images[0].link}
-                          alt={product.title}
-                          width={100}
-                          height={100}
-                        />
-                      </div>
-
-                      <div className={styles.order__itemContent}>
-                        <div className={styles.order__itemHeader}>
-                          <h4 className={styles.order__itemTitle}>
-                            {product.title}
-                          </h4>
-
-                          <button
-                            className={styles.order__itemDelete}
-                            type="button"
-                            onClick={() => deleteFromStore(product.id)}
-                          ></button>
-                        </div>
-
-                        <div className={styles.order__itemFooter}>
-                          <p className={styles.order__itemCode}>
-                            code: {product.code}
-                          </p>
-
-                          <p className={styles.order__itemPrice}>
-                            {convertPriceToReadable(
-                              product.totalPrice,
-                              currency,
-                              locale,
-                            )}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className={styles.order__itemButtons}>
-                      <div className={styles.order__itemQuantity}>
-                        <div className={styles.order__itemQuantityBtn}>
-                          <button
-                            onClick={() => decreaseItemQuantity(product.id)}
-                          >
-                            -
-                          </button>
-                        </div>
-                        <p>{product.quantity}</p>
-                        <div className={styles.order__itemQuantityBtn}>
-                          <button
-                            onClick={() => increaseItemQuantity(product.id)}
-                          >
-                            +
-                          </button>
-                        </div>
-                      </div>
-
-                      <div className={styles.order__itemQuantityBtnFavorite}>
-                        <HeartIcon
-                          onClick={handleClickLike}
-                          isLiked={isLiked}
-                        />
-                      </div>
-                    </div>
-                  </article>
-                </li>
+                <ProductCardForOrder key={product.id} product={product} />
               ))}
             </ul>
 
