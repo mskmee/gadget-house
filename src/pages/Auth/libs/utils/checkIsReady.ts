b1ee79c3fc81c0e89e83b3ee57b9474b7ad297be
Serhii Svitlychnyi <svetlichniy1905@gmail.com>
2025-03-17 19:54:07 +0200

    removed an unused utility

--- src/pages/Auth/libs/utils/checkIsReady.ts
+++
@@ -1,5 +0,0 @@
-import { ForgotFormDto, LoginFormDto, RegisterFormDto } from "../types/form-dto";
-
-export const checkIsReady = (form: Partial<LoginFormDto | RegisterFormDto | ForgotFormDto>): boolean => {
-  return Object.values(form).every((value) => value.trim() !== '');
-};
