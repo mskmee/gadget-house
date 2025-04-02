0a8003903de90301a78b42f9b575eb6009107b3b
Rozygeldi Halykov <rozygeldi1994@gmail.com>
2025-04-02 21:46:29 +0300

    Update Main.tsx
    Signed-off-by: Rozygeldi Halykov <rozygeldi1994@gmail.com>

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,10 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-
-import { DEFAULT_PAGE, DEFAULT_SIZE } from '@/constants/pagination';
 import {
   LocalStorageKey,
   localStorageService,
 } from '@/utils/packages/local-storage';
 import { useTypedSelector, useActions } from '@/hooks/hooks';
-import { useMediaQuery } from 'react-responsive';
 import { getUserData } from '@/store/auth/actions';
 import { AppDispatch } from '@/store';
 import { AppRoute, DataStatus } from '@/enums/enums';
