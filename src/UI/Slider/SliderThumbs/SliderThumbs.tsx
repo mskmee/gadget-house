194653ec21b54c8cf40361c5b934e15bde09cf20
Dasha <dasha2312s3@gmail.com>
2025-06-04 18:40:54 +0300

    slider styles

--- src/UI/Slider/SliderThumbs/SliderThumbs.tsx
+++ src/UI/Slider/SliderThumbs/SliderThumbs.tsx
@@ -13,48 +13,52 @@ function SliderThumbs({data, prevArrow, nextArrow, currentSlide, classNameThumb,
 
   return (
     <>
-      <SliderBase 
-        onSwiper={setMainSwiper}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs, Pagination]}
-        className={classNames('mainSlider', classNameMain)}
-        pagination={isMobile ?? true}
-        prevArrow={prevArrow}
-        nextArrow={nextArrow}
-        initialSlide={(currentSlide?.id ?? 1) - 1}
-        onSlideChange={({ realIndex }: { realIndex: number }) => {
-          if (onSlideChange) {
-            onSlideChange(realIndex);
-          }
-        }}
-      >
-        {data.map((image, index) => (
-          <div style={{ cursor: 'pointer' }} key={index}>
-            <img src={image.link} alt={`main-slide-${index}`} />
-          </div>
-        ))}
-      </SliderBase>
+      <div className='w-100'>
+        <SliderBase 
+          onSwiper={setMainSwiper}
+          spaceBetween={10}
+          navigation={true}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Navigation, Thumbs, Pagination]}
+          className={classNames('mainSlider', classNameMain)}
+          pagination={isMobile ?? true}
+          prevArrow={prevArrow}
+          nextArrow={nextArrow}
+          initialSlide={(currentSlide?.id ?? 1) - 1}
+          onSlideChange={({ realIndex }: { realIndex: number }) => {
+            if (onSlideChange) {
+              onSlideChange(realIndex);
+            }
+          }}
+        >
+          {data.map((image, index) => (
+            <div style={{ cursor: 'pointer' }} key={index}>
+              <img src={image.link} alt={`main-slide-${index}`} />
+            </div>
+          ))}
+        </SliderBase>
+      </div>
 
-      {!isMobile && (<SliderBase
-        onSwiper={setThumbsSwiper}
-        spaceBetween={spaceBetween}
-        navigation={false}
-        pagination={false}
-        watchSlidesProgress={true}
-        breakpoints={breakpointsThumbs}
-        slidesPerView={slidesPerView}
-        initialSlide={(currentSlide?.id ?? 1) - 1}
-        modules={[FreeMode, Navigation, Thumbs, Pagination]}
-        className={classNames('thumb-wrapper', classNameThumb)}
-      >
-        {data.map((image, index) => (
-          <div style={{ cursor: 'pointer' }} key={index}>
-            <img src={image.link} alt={`main-slide-${index}`} />
-          </div>
-        ))}
-      </SliderBase>)}
+      {!isMobile && (
+        <SliderBase
+          onSwiper={setThumbsSwiper}
+          spaceBetween={spaceBetween}
+          navigation={false}
+          pagination={false}
+          watchSlidesProgress={true}
+          breakpoints={breakpointsThumbs}
+          slidesPerView={slidesPerView}
+          initialSlide={(currentSlide?.id ?? 1) - 1}
+          modules={[FreeMode, Navigation, Thumbs, Pagination]}
+          className={classNames('thumb-wrapper', classNameThumb)}
+        >
+          {data.map((image, index) => (
+            <div style={{ cursor: 'pointer' }} key={index} className='thumb__item'>
+              <img src={image.link} alt={`main-slide-${index}`} />
+            </div>
+          ))}
+        </SliderBase>
+      )}
     </>
   );
 }
