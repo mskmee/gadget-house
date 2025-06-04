45aa606c2554bb627c8e89b1a08f88f24d8c0470
Dasha <dasha2312s3@gmail.com>
2025-06-04 22:22:24 +0300

    fixed error from console. need check it

--- src/utils/packages/singleProduct/type/singleProduct-api.interface.ts
+++ src/utils/packages/singleProduct/type/singleProduct-api.interface.ts
@@ -1,7 +1,9 @@
 import { AddReviewRequestDTO, AddReviewResponseDTO, GetReviewResponseDTO } from "./types"
 
 interface ISingleProductApi {
+  // eslint-disable-next-line no-unused-vars
   addReview:(data: AddReviewRequestDTO) => Promise<AddReviewResponseDTO>;
+  // eslint-disable-next-line no-unused-vars
   getReviews: ({productId, page}: {productId: number, page: number}) => Promise<GetReviewResponseDTO>
 }
 
