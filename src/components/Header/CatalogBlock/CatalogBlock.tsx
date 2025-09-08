1dff778281524ac8455afb8588e95bf2bd3fceb5
pavlo1377 <pavlo.antohiv@gmail.com>
2025-09-08 18:46:18 +0300

    fix: closing cataloglist after clicking on cataloglist bottom buttons

--- src/components/Header/CatalogBlock/CatalogBlock.tsx
+++ src/components/Header/CatalogBlock/CatalogBlock.tsx
@@ -1,42 +1,54 @@
-import { useMediaQuery } from "react-responsive";
+/* eslint-disable no-unused-vars */
+import { useMediaQuery } from 'react-responsive';
 import { BurgerMenuIcon, CatalogIcon, LeftArrow } from '@/assets/constants';
-import React from "react";
-import classNames from "classnames";
-import { CatalogList } from "@/components/BurgerMenu/CatalogList";
+import React from 'react';
+import classNames from 'classnames';
+import { CatalogList } from '@/components/BurgerMenu/CatalogList';
 import styles from '../header.module.scss';
 
 interface CatalogBlockProps {
-  isCatalogListOpen: boolean,
-  setIsCatalogListOpen: React.Dispatch<React.SetStateAction<boolean>>,
-  // eslint-disable-next-line no-unused-vars
+  isCatalogListOpen: boolean;
+  setIsCatalogListOpen: React.Dispatch<React.SetStateAction<boolean>>;
   openCatalogOnFocus?: (e: React.FocusEvent<HTMLButtonElement>) => void;
-  onAuthClick?: () => void,
-  openCatalog?: () => void,
-  // eslint-disable-next-line no-unused-vars
-  closeCatalog?: (e: React.MouseEvent<HTMLButtonElement | HTMLDivElement | KeyboardEvent | HTMLAnchorElement>) => void;
+  onAuthClick?: () => void;
+  openCatalog?: () => void;
+  closeCatalog?: (
+    e: React.MouseEvent<
+      HTMLButtonElement | HTMLDivElement | KeyboardEvent | HTMLAnchorElement
+    >,
+  ) => void;
 }
 
-const CatalogBlock = ({ isCatalogListOpen, setIsCatalogListOpen, onAuthClick, openCatalogOnFocus, openCatalog, closeCatalog }: CatalogBlockProps) => {
-  
+const CatalogBlock = ({
+  isCatalogListOpen,
+  setIsCatalogListOpen,
+  onAuthClick,
+  openCatalogOnFocus,
+  openCatalog,
+  closeCatalog,
+}: CatalogBlockProps) => {
   const isLessThan992px = useMediaQuery({
     query: '(max-width: 992px)',
   });
-  
+
   const handleOpenCloseBurgerMenu = () => {
     document.body.style.overflow = 'hidden';
-    setIsCatalogListOpen(prev => !prev)
+    setIsCatalogListOpen((prev) => !prev);
   };
 
   return (
     <>
       {isLessThan992px ? (
-        <button type="button" onClick={handleOpenCloseBurgerMenu} className={styles.catalog__hamburger}>
+        <button
+          type="button"
+          onClick={handleOpenCloseBurgerMenu}
+          className={styles.catalog__hamburger}
+        >
           <img
             src={isCatalogListOpen ? LeftArrow : BurgerMenuIcon}
             className={classNames({
               [styles['active-burger-menu']]: isCatalogListOpen,
             })}
-            
           />
         </button>
       ) : (
@@ -56,13 +68,15 @@ const CatalogBlock = ({ isCatalogListOpen, setIsCatalogListOpen, onAuthClick, op
       {isCatalogListOpen && (
         <div
           className={classNames(styles.catalogListWrap)}
-      >
-        <CatalogList
-          onAuthClick={onAuthClick}
-          isCatalogListOpen={isCatalogListOpen}
-          closeCatalog={closeCatalog}
-        />
-      </div>)}
+          onClick={handleOpenCloseBurgerMenu}
+        >
+          <CatalogList
+            onAuthClick={onAuthClick}
+            isCatalogListOpen={isCatalogListOpen}
+            closeCatalog={closeCatalog}
+          />
+        </div>
+      )}
     </>
   );
 };
