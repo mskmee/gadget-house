1d0e66b92ad20604abbccc315ba7f92f1518d6cf
VolodymyrNakonechnyi <volodymyr.nakonechnyi28@gmail.com>
2025-10-09 14:24:40 +0300

    fix: phone number validation

--- src/utils/helpers/formatPhoneNumber.ts
+++ src/utils/helpers/formatPhoneNumber.ts
@@ -22,5 +22,9 @@ export const formatPhoneInput = (value: string): string => {
 
 export const formatPhoneDisplay = (phone: string): string => {
   const digits = phone.replace(/\D/g, '');
+  if (digits.length !== 10) {
+    return '';
+  }
+
   return digits.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, '($1) $2 $3 $4');
 };
