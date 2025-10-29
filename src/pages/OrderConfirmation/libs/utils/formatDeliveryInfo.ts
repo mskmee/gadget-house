5655b50cc190b481115775c99e5937ffc62921ed
pavlo1377 <pavlo.antohiv@gmail.com>
2025-10-29 21:22:41 +0200

    bugfix: phone number validation & bracket deletion

--- src/pages/OrderConfirmation/libs/utils/formatDeliveryInfo.ts
+++ src/pages/OrderConfirmation/libs/utils/formatDeliveryInfo.ts
@@ -14,7 +14,7 @@ const formatDeliveryInfo = (initialValues: {
   if (deliveryType === 'NOVA') {
     parts.push(`Nova Poshta`);
   } else if (deliveryType === 'UKRPOSHTA') {
-    parts.push(`UkrPoshta}`);
+    parts.push(`UkrPoshta`);
   } else if (deliveryType === 'COURIER') {
     parts.push(`By courier `);
   }
