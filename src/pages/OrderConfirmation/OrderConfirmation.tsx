93deb417c516f2a55fe1df184304938c25716c28
Deepanshi <deepanshibansal99@gmail.com>
2026-01-16 06:37:32 +0000

    Linting fix

--- src/pages/OrderConfirmation/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation/OrderConfirmation.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useMemo, useState } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import cn from 'classnames';
 
